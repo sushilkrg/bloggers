2e755a63c2c8e8893fe901b2b99e870d6e47e1d9
Sushil Kumar <sushilkr820@gmail.com>
2023-10-16 13:56:01 +0530

    created blog app with multiple functionality and managed backend with appwrite service and frontend with react

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import Container from "./container/Container";
+import Logo from "./logo/Logo";
+import LogoutBtn from "./header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
+import Select from "./Select";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Button,
+  Input,
+  Select,
+  RTE,
+  Signup,
+  Login,
+  PostForm,
+  PostCard,
+  AuthLayout,
+};
